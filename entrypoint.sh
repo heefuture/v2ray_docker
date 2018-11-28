211d67f7e712bdf3b5a8ab535ea51ffefe7a7547
heefuture <heefuture@163.com>
2018-11-28 18:14:18 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+cd /v2ray
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+unzip v2ray.zip 
+
+#old version
+#mv /v2ray/v2ray-v$VER-linux-64/v2ray .
+#mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
+#mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
+#mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
+
+mv /v2ray/v2ray .
+mv /v2ray/v2ctl .
+mv /v2ray/geoip.dat .
+mv /v2ray/geosite.dat .
+mv /v2ray/config.json .
+
+chmod +x v2ray v2ctl
+echo -e "$CONFIG_JSON" > config.json
+./v2ray
